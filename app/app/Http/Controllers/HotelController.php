6bdc39a7f4dddff8ce386ae8510f3851809a9640
大城亮 <hiltoc.katsu@gmail.com>
2025-03-22 20:13:40 +0900

    hotelsテーブルの一覧表示実装

---
+++ app/app/Http/Controllers/HotelController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hotel;
+use Illuminate\Http\Request;
+
+class HotelController extends Controller
+{
+    public function index()
+    {
+        $hotels = Hotel::all();
+        return view('hotels.index', compact('hotels'));
+    }
+}
